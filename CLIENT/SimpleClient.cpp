2d56df726e74cf2e69ce3cdef9eeaa3658e1c7c2
L3o <leogravillon@hotmail.fr>
2024-04-29 10:43:16 +0200

    [add] client/server message and response

--- CLIENT/SimpleClient.cpp
+++ CLIENT/SimpleClient.cpp
@@ -11,6 +11,45 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	cout << "Thank you for using ChArtFX !\n";
+	IPaddress ip;
+	if (SDLNet_ResolveHost(&ip, "localhost", 4242) == -1) {
+		cerr << "SDLNet_ResolveHost error: " << SDLNet_GetError() << endl;
+	}
+
+	TCPsocket clientSocket = SDLNet_TCP_Open(&ip);
+	if (!clientSocket) {
+		cerr << "TCP Open error: " << SDLNet_GetError() << endl;
+		SDLNet_Quit();
+		return 1;
+	}
+
+	string message = "Hello there !";
+
+	int bytesSent = SDLNet_TCP_Send(clientSocket, message.c_str(), message.length() + 1);
+	if (bytesSent < message.length() + 1) {
+		cerr << "SDLNet TCP Send error: " << SDLNet_GetError() << endl;
+		SDLNet_TCP_Close(clientSocket);
+		SDLNet_Quit();
+		return 1;
+	}
+
+	cout << "Sent: " << bytesSent << " bytes to the server !" << endl;
+
+	char buffer[1024];
+	int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+	if (bytesRead <= 0) {
+		cerr << "SDL Net TCP Recv error: " << SDLNet_GetError() << endl;
+		SDLNet_TCP_Close(clientSocket);
+		SDLNet_Quit();
+		return 1;
+			
+	}
+	else {
+		cout << "Received: " << bytesRead << " bytes from the server !" << endl;
+		cout << "Message: " << buffer << endl;
+	}
+
+	SDLNet_TCP_Close(clientSocket);
+	SDLNet_Quit();
 	return 0;
 }
--- SERVER/SimpleServer.cpp
+++ SERVER/SimpleServer.cpp
@@ -11,5 +11,37 @@ int main(int argc, char* argv[]) {
 	}
 
 	cout << "Thank you for using ChArtFX !\n";
+	IPaddress ip;
+	if (SDLNet_ResolveHost(&ip, nullptr, 4242) == -1) {
+		cerr<< "Resolve Host error: " << SDLNet_GetError() << endl;
+		SDLNet_Quit();
+		return 1;
+	}
+
+	TCPsocket serverSocket = SDLNet_TCP_Open(&ip);
+	if (!serverSocket) {
+		cerr << "TCP Open error : " << SDLNet_GetError() << endl;
+		SDLNet_Quit();
+		return 1;
+	}
+	
+	TCPsocket clientSocket;
+	while (true) {
+		clientSocket = SDLNet_TCP_Accept(serverSocket);
+		if (clientSocket) {
+			cout << "A client reached the server!" << endl;
+			char buffer[1024];
+			int bytesRead = SDLNet_TCP_Recv(clientSocket, buffer, sizeof(buffer));
+			if (bytesRead > 0) {
+				cout << "Incomming message: " << buffer << endl;
+				string answer = "General Kenobi !";
+				int bytesSent = SDLNet_TCP_Send(clientSocket, answer.c_str(), answer.size() + 1);
+				if (bytesSent < answer.size() + 1) {
+					cerr << "SDLNet TCP Send error: " << SDLNet_GetError() << endl;
+					break;
+				}
+			}
+		}
+	}
 	return 0;
 }
